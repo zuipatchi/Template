6f3878e5413f6081b37bb60c8c959abf8d6c402b
zuipatchi <zuipatchi@gmail.com>
2026-02-06 12:59:06 +0900

    タイトルシーン

---
+++ Assets/Scripts/Title/TitleLifetimeScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Scripts.Title
+{
+
+    public class TitleLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
